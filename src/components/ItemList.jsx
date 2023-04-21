b1a639b5451db26b19b33be6a70fe4847b1088c0
Tomas Juarez <mokajua@gmail.com>
2023-04-21 19:36:39 -0300

    item list refactory

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,94 +1,94 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { Container, display, maxWidth } from "@mui/system";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Container, Box } from "@mui/material";
 import { Item } from "./Item";
-import { Box } from "@mui/material";
-import "../css/listado.css"
 import { useParams } from "react-router-dom";
-import iconoBebida from "../assets/bebida.png"
-import iconoEntrada from "../assets/entrada.png"
-import iconoPostre from "../assets/postre.png"
-import iconoPrincipal from "../assets/principal.png"
+import iconoBebida from "../assets/bebida.png";
+import iconoEntrada from "../assets/entrada.png";
+import iconoPostre from "../assets/postre.png";
+import iconoPrincipal from "../assets/principal.png";
+import "../css/listado.css";
 
-//https://rafalopez.000webhostapp.com/api/V1/MESA/PRODUCTOS
+export const ItemList = () => {
+  const [menu, setMenu] = useState([]);
+  const [category, setCategory] = useState("");
 
-export const ItemList = ()=>{
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          "https://rafalopez.000webhostapp.com/api/V1/MESA/PRODUCTOS"
+        );
 
-    
-    const [menu , setMenu]= useState([])
-    const [category,setCategory] = useState("")
-    
-    useEffect(() => {
-        try {
-            axios.get("https://rafalopez.000webhostapp.com/api/V1/MESA/PRODUCTOS")
-        .then(res=>{
-            if (category == "") {
-                const data = res.data;
-                setMenu(data)
-                console.log( data)
-            }else{
-                const data = res.data ;
-                setMenu(data.filter(e=>e.categoria === category))
-            }
-            
+        if (category === "") {
+          setMenu(res.data);
+        } else {
+          setMenu(res.data.filter((e) => e.categoria === category));
         }
-        ).catch(error =>{
-            console.log(error.message)
-        })
-            
-        } catch (error) {
-            console.log(error.message)
-        }
-    },[category]); 
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
 
+    fetchData();
+  }, [category]);
 
-    const handleCategoryEvent = (string)=>{
-        setCategory(string)
-       
-    }
-    
+  const handleCategoryEvent = (string) => {
+    setCategory(string);
+  };
 
-        
-        
-    return(
-        <>
-        
-            <Container style={{overflow:"scroll"}}  className="listContainer"  maxWidth="md">
-            <Box sx={{display:"flex",}}> 
-                <div className="buttonCartegoryContainer">
-                    <button onClick={()=>{handleCategoryEvent("entrada")}} className='buttonCartegory' style={{backgroundImage:`url(${iconoEntrada})`}}></button>
-                  
-                    <span style={{color:"#ffff",textAlign:"center"}}>entrada</span>
-                </div>
-                <div  className="buttonCartegoryContainer">
-                    <button  onClick={()=>{handleCategoryEvent("principal")}} className='buttonCartegory' style={{backgroundImage:`url(${iconoPrincipal})`}}></button>
-                    <span style={{color:"#ffff",textAlign:"center"}}> principal</span>
-                </div>
-                <div  className="buttonCartegoryContainer">
-                <button  onClick={()=>{handleCategoryEvent("postre")}} className='buttonCartegory' style={{backgroundImage:`url(${iconoPostre})`}}></button>
-                <span style={{color:"#ffff",textAlign:"center"}}>postre</span>
-                </div>
-                <div  className="buttonCartegoryContainer">
-                    <button  onClick={()=>{handleCategoryEvent("bebida")}} className='buttonCartegory' style={{backgroundImage:`url(${iconoBebida})`}} ></button>
-                    <span style={{color:"#ffff",textAlign:"center"}}>bebida</span>
-                </div>
-            </Box>
-                <Box className="list-scroll">
+  return (
+    <Container style={{ overflow: "scroll" }} className="listContainer" maxWidth="md">
+      <Box sx={{ display: "flex" }}>
+        <div className="buttonCartegoryContainer">
+          <button
+            onClick={() => {
+              handleCategoryEvent("entrada");
+            }}
+            className="buttonCartegory"
+            style={{ backgroundImage: `url(${iconoEntrada})` }}
+          ></button>
 
-                    {menu.length > 0 ? menu.map((item)=>{
-                        return(
-                            
-                            <Item  key={item.id} {...item}></Item>
-                        )
-                    } ) : <h1>Loading...</h1>}
-
-                </Box>
-
-                
-
-            </Container>
-       
-        </>
-    );
-
-}
+          <span style={{ color: "#ffff", textAlign: "center" }}>entrada</span>
+        </div>
+        <div className="buttonCartegoryContainer">
+          <button
+            onClick={() => {
+              handleCategoryEvent("principal");
+            }}
+            className="buttonCartegory"
+            style={{ backgroundImage: `url(${iconoPrincipal})` }}
+          ></button>
+          <span style={{ color: "#ffff", textAlign: "center" }}> principal</span>
+        </div>
+        <div className="buttonCartegoryContainer">
+          <button
+            onClick={() => {
+              handleCategoryEvent("postre");
+            }}
+            className="buttonCartegory"
+            style={{ backgroundImage: `url(${iconoPostre})` }}
+          ></button>
+          <span style={{ color: "#ffff", textAlign: "center" }}>postre</span>
+        </div>
+        <div className="buttonCartegoryContainer">
+          <button
+            onClick={() => {
+              handleCategoryEvent("bebida");
+            }}
+            className="buttonCartegory"
+            style={{ backgroundImage: `url(${iconoBebida})` }}
+          ></button>
+          <span style={{ color: "#ffff", textAlign: "center" }}>bebida</span>
+        </div>
+      </Box>
+      <Box className="list-scroll">
+        {menu.length > 0 ? (
+          menu.map((item) => <Item key={item.id} {...item} />)
+        ) : (
+          <h1>Loading...</h1>
+        )}
+      </Box>
+    </Container>
+  );
+};
